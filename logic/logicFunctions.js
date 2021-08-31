cb03b3c3b8692ccfe4dea39c92776aecfbd00192
Manuel Hermes <m.hermes.git@gmail.com>
2021-08-31 12:03:58 +0200

    updated ip stealthing function

--- logic/logicFunctions.js
+++ logic/logicFunctions.js
@@ -3,58 +3,69 @@ const dbClient = require('../config/database');
 let globalCounter = 0;
 
 function increaseCounter() {
-  return new Promise(
-    (resolve, reject) => {
-      resolve(++globalCounter);
-    }
-  )
+	return new Promise(
+		(resolve, reject) => {
+			resolve(++globalCounter);
+		}
+	)
 }
 
 function getCounter() {
-  return globalCounter;
+	return globalCounter;
 }
 
 async function updateAdressTable(newAdress) {
 
-  try {
-    await dbClient.query('begin');
+	try {
+		await dbClient.query('begin');
 
-    const insertQText =
-      'insert into public.ip_adresses ' +
-      '(adress) values ($1);';
-    const insertQValue = [newAdress];
+		const insertQText =
+		'insert into public.ip_adresses ' +
+		'(adress) values ($1);';
+		const insertQValue = [newAdress];
 
-    await dbClient.query(insertQText, insertQValue);
+		await dbClient.query(insertQText, insertQValue);
 
-    const selectQText =
-      'select adress from public.ip_adresses ' +
-      'order by id desc ' +
-      'limit 10';
+		const selectQText =
+		'select adress from public.ip_adresses ' +
+		'order by id desc ' +
+		'limit 10';
 
-    const newList = dbClient.query({text: selectQText, rowMode: 'array'});
+		const newList = dbClient.query({
+			text: selectQText,
+			rowMode: 'array'
+		});
 
-    await dbClient.query('COMMIT');
+		await dbClient.query('COMMIT');
 
-    return await newList;
-  }
-	catch (e) {
-    await dbClient.query('ROLLBACK');
-  }
+		return await newList;
+	} catch (e) {
+		await dbClient.query('ROLLBACK');
+	}
 
 }
 
 function anonymizeIp(ip) {
-  if (ip.length < 4) {
-    return '???';
-  } else {
-		let newIp = ip.slice(0, ip.length-3) + '???';
-		return newIp;
-  }
+	if (ip.length < 4) {
+		return '???';
+	} else {
+		if (let dotPos = ip.lastIndexOf('.') !== -1)) {
+			// ip adress contains a dot
+			let newIp = ip.slice(0, dotPos) + '???';
+			return newIp;
+		} else if (let colon = ip.lastIndexOf(':') !== -1) {
+			// ip adress contains a colon
+			newIp = ip.slice(0, dotPos) + '???';
+		} else {
+			// replace last 3 digits
+			let newIp = ip.slice(0, ip.length - 3) + '???';
+		}
+	}
 }
 
 module.exports = {
-  increaseCounter,
-  getCounter,
-  updateAdressTable,
+	increaseCounter,
+	getCounter,
+	updateAdressTable,
 	anonymizeIp
 }
